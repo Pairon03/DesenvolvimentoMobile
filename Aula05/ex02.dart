cc6a2eeaebd5b8bd87ae7f840a1cf673d964fe59
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-02-27 19:49:22 -0300

    ex02

---
+++ Aula05/ex02.dart
@@ -0,0 +1,45 @@
+/*
+Exemplo 1 - Programação orientada a objeto
+27.02.2025
+*/
+
+class Carro {
+  String marca = "Nissan";
+  int ano = 2025;
+
+  // cria metodo abrir
+
+  void abrirporta(int qtde_p) {
+    if (qtde_p == 1) {
+      print("Porta do motorista aberta");
+    } else if (qtde_p == 2) {
+      print("Porta do motorista e do passageiro aberta");
+    } else if (qtde_p == 3) {
+      print("Porta do motorista, do passageiro e traseira direita aberta");
+    } else {
+      print("Todas as portas do carro aberta");
+    }
+  }
+
+  void fecharporta(int qtde_p) {
+    if (qtde_p == 1) {
+      print("Porta do motorista fechada");
+    } else if (qtde_p == 2) {
+      print("Porta do motorista e do passageiro fechada");
+    } else if (qtde_p == 3) {
+      print("Porta do motorista, do passageiro e traseira direita fechada");
+    } else {
+      print("Todas as portas do carro fechada");
+    }
+  }
+
+  void ligar() {
+    print("Carro ligado");
+  }
+
+  void desligar() {
+    print("Carro desligado");
+  }
+
+  void main() {}
+}
