87727cfa18338ed7701b956a92c45a87d7cdbf28
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-02-27 20:34:16 -0300

    ex05.

--- Aula05/ex05.dart
+++ Aula05/ex05.dart
@@ -19,3 +19,23 @@ class Cachorro extends Animal {
     print("O animal ${nome} esta latindo");
   }
 }
+
+class Tigre extends Animal {
+  String? cor;
+
+  void atacar() {
+    print("O animal Tigre $nome atacou");
+  }
+}
+
+void main() {
+  Cachorro Rocky = Cachorro();
+  Rocky.nome = "Rocky";
+  Rocky.idade = 2;
+  Rocky.latir();
+  Rocky.dormir();
+  Tigre Memphis = Tigre();
+  Memphis.nome = "Memphis";
+  Memphis.idade = 30;
+  Memphis.atacar();
+}
