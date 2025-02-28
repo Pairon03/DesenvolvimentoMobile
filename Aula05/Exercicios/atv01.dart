d9512f7d63ca338444caea4783f968215ac1bc82
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-02-27 21:44:21 -0300

    atv 01

---
+++ Aula05/Exercicios/atv01.dart
@@ -0,0 +1,29 @@
+/*
+Exercícios
+27.02.2025
+*/
+
+//  Classe mae
+class Animal {
+  String? nome;
+  int? idade;
+  String? cor;
+  String? raca;
+
+  void dormir() {
+    print("O animal esta dormindo");
+  }
+}
+
+void main() {
+  // instancio o objeto chamado minhaCasa
+  Animal meuCachorro = Animal();
+  meuCachorro.nome = "Thor";
+  meuCachorro.idade = 3;
+  meuCachorro.cor = "Marrom";
+  meuCachorro.raca = "Vira-lata";
+  print("Meu cachorro se chama ${meuCachorro.nome}");
+  print("Ele tem ${meuCachorro.idade} anos");
+  print("Meu cachorro é ${meuCachorro.cor}");
+  print("Meu cachorro é um ${meuCachorro.raca}");
+}
