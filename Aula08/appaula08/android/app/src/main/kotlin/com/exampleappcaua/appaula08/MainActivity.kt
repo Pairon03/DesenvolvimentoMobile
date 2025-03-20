d112127106c3610b2053929fc18eebe86a668762
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-03-20 20:30:21 -0300

    app Aula08

---
+++ Aula08/appaula08/android/app/src/main/kotlin/com/exampleappcaua/appaula08/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleappcaua.appaula08
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Aula08/appaula08a2/android/app/src/main/kotlin/com/exampleappcaua/appaula08a2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleappcaua.appaula08a2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
