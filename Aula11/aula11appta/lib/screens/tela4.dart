3ddb33dc34342625e40d76d9b471a1f83ab909e1
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-04-10 20:58:23 -0300

    chatbot

--- Aula11/aula11appta/lib/screens/tela4.dart
+++ Aula11/aula11appta/lib/screens/tela4.dart
@@ -82,11 +82,13 @@ class _ChatScreenState extends State<ChatScreen> {
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
             child: Row(
               children: [
-                TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                      hintText: 'Digite sua messagem',
-                      border: InputBorder.none),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                        hintText: 'Digite sua mensagem',
+                        border: InputBorder.none),
+                  ),
                 ),
                 IconButton(
                     onPressed: _sendMessage,
@@ -127,7 +129,7 @@ class MessageBubble extends StatelessWidget {
             margin: EdgeInsets.symmetric(vertical: 5),
             padding: EdgeInsets.all(12),
             decoration: BoxDecoration(
-                color: isMe ? Colors.green[10] : Colors.grey[30],
+                color: isMe ? Colors.green[100] : Colors.grey[300],
                 borderRadius: BorderRadius.circular(12)),
             child: Text(text),
           ),
