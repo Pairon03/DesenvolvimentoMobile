9d044e9a6317d17434650c99327871868075eb73
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-05-08 22:14:11 -0300

    AULA 14

--- Aula14/appaula14_ta/lib/screens/cadastroproduto.dart
+++ Aula14/appaula14_ta/lib/screens/cadastroproduto.dart
@@ -1,3 +1,4 @@
+import 'package:appaula14ta/screens/prod_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -16,60 +17,61 @@ class _CadastroprodutoState extends State<Cadastroproduto> {
   TextEditingController qte = TextEditingController();
 
   // cria funçao para cadastro do produto
-  _cadastrarproduto()async{
+  _cadastrarproduto() async {
     String url = "http://10.109.83.16:3000/produtos";
     // cria estrutura da mensagem para cadastro dos produtos
-    Map<String,dynamic>prod={
-      "id":nomeprod.text,
-      "nome":nomeprod.text,
-      "valor":valorprod.text,
-      "qtde":qte.text
+    Map<String, dynamic> prod = {
+      "id": nomeprod.text,
+      "nome": nomeprod.text,
+      "valor": valorprod.text,
+      "qtde": qte.text
     };
     await http.post(Uri.parse(url),
-    headers: <String,String>{
-    'Content-type':'application/json; charset=UTF-8'},
-    body: jsonEncode(prod)
-    );
-    nomeprod.text="";
-    valorprod.text="";
-    qte.text="";
+        headers: <String, String>{
+          'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(prod));
+    nomeprod.text = "";
+    valorprod.text = "";
+    qte.text = "";
     // função para deletar um produto
     showDialog(
-        context: context, 
-        builder: (BuildContext){
+        context: context,
+        builder: (BuildContext) {
           return AlertDialog(
             content: Text("Produto ${nomeprod.text} cadastrado !"),
             actions: [
               TextButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                }, child: Text("Fechar"))
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Fechar"))
             ],
           );
-
         });
   }
-    _deletarproduto(){
-      // metodo para deletar um produto
-      http.delete(Uri.parse("http://10.109.83.16:3000/produtos/${nomeprod.text}"));
-      nomeprod.text="";
-      showDialog(
-        context: context, 
-        builder: (BuildContext){
+
+  _deletarproduto() {
+    // metodo para deletar um produto
+    http.delete(
+        Uri.parse("http://10.109.83.16:3000/produtos/${nomeprod.text}"));
+    nomeprod.text = "";
+    showDialog(
+        context: context,
+        builder: (BuildContext) {
           return AlertDialog(
             content: Text("Produto ${nomeprod.text} deletado !"),
             actions: [
               TextButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                }, child: Text("Fechar"))
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Fechar"))
             ],
           );
-
         });
-    }
+  }
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,42 +86,44 @@ class _CadastroprodutoState extends State<Cadastroproduto> {
             child: TextField(
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8)
-                ),
-                hintText: "Digite o nome do produto"
-              ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  hintText: "Digite o nome do produto"),
               controller: nomeprod,
             ),
           ),
-           Padding(
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8)
-                ),
-                hintText: "Digite o valor do produto"
-              ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  hintText: "Digite o valor do produto"),
               controller: valorprod,
             ),
           ),
-           Padding(
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8)
-                ),
-                hintText: "Digite a qtde do produto"
-              ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  hintText: "Digite a qtde do produto"),
               controller: qte,
             ),
           ),
-          ElevatedButton(onPressed: _cadastrarproduto, child: Text("Cadastrar produto")),
-          ElevatedButton(onPressed: _deletarproduto, child: Text("Deletar produto")),
+          ElevatedButton(
+              onPressed: _cadastrarproduto, child: Text("Cadastrar produto")),
+          ElevatedButton(
+              onPressed: _deletarproduto, child: Text("Deletar produto")),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Produto_screen()));
+              },
+              child: Text("Produtos screen"))
         ],
       ),
     );
