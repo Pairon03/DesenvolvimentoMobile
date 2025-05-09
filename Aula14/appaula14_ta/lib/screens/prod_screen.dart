5efc4dcc83bceffbee57d06741953a0fd05d54af
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-05-08 21:53:22 -0300

    Cadastro Produto

--- Aula14/appaula14_ta/lib/screens/prod_screen.dart
+++ Aula14/appaula14_ta/lib/screens/prod_screen.dart
@@ -13,13 +13,56 @@ class _Produto_screenState extends State<Produto_screen> {
   // função para exibir os dados assim que a tela for carregada
   void initState() {
     super.initState();
+    leituradados();
   }
 
   // cria variavel dado
   List dado = [];
+  Future<void> leituradados() async {
+    String url = "http://10.109.83.16:3000/produtos";
+    http.Response resposta = await http.get(Uri.parse(url));
+
+    if (resposta.statusCode == 200) {
+      setState(() {
+        dado = jsonDecode(resposta.body) as List<
+            dynamic>; // conversao dos produtos para uma lista no formato json
+        print(dado);
+      });
+    } else {
+      print(resposta.statusCode);
+      throw Exception('Falha ao consumir API');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("App Ecommerce - Produtos"),
+      ),
+      body: Center(
+        child: ListView.builder(
+            itemCount: dado.length,
+            itemBuilder: (context, index) {
+              final item = dado[index];
+              return ListTile(
+                title: Text(
+                  "Nome ${item["nome"]}",
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+                subtitle: Column(
+                  children: [
+                    Text("Valor: R\$ ${item["valor"]}",
+                        style: TextStyle(fontSize: 18)),
+                    Text("Qtde: ${item["qtde"]}",
+                        style: TextStyle(fontSize: 18))
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
